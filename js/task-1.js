1a66b6b6456159645d2252769fc12900b5a33a7a
Yuliia Hubska <zhukapuka10@gmail.com>
2025-02-07 11:23:53 +0200

    fix

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,9 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  let totalOrder = quantity * pricePerDroid;
-  if (totalOrder <= customerCredits) {
-    return `"You ordered ${quantity} droids worth ${totalOrder} credits!"`;
-  } else {
-    return "Insufficient funds!";
-  }
+"use strict";
+function slugify(title) {
+  return title.toLowerCase().split(" ").join("-");
 }
-//mentor checkout
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!
+//mentor check
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
